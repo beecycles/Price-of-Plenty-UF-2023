8ec9645afc2b74826a57a747815c2c088ac9abae
Joan Meiners <joan.meiners@gmail.com>
2023-02-09 22:37:54 -0700

    Add files via upload

--- Fertilizer Files Code.R
+++ Fertilizer Files Code.R
@@ -11,6 +11,11 @@ library(dplyr)
 library(plyr)
 library(tidyverse)
 library(ggplot2)
+library(MASS) 
+library(reshape2) 
+library(reshape) 
+library(quantreg)
+
 
 # prep data
 # Step 1: Download from https://www.fdacs.gov/Agriculture-Industry/Fertilizer-Licensing-and-Tonnage-Reporting and convert from pdf to xlsx
@@ -56,9 +61,37 @@ FLFert_all = join(FLFert.21.22, FLFert.20.21, by = "ID")
 FLFert_all = join(FLFert_all, FLFert.19.20, by = "ID")
 FLFert_all = join(FLFert_all, FLFert.18.19, by = "ID")
 FLFert_all = join(FLFert_all, FLFert.17.18, by = "ID")
+names(FLFert_all) <- sub("^X", "", names(FLFert_all))
 View(FLFert_all)
 dim(FLFert_all) # 456 36
+colnames(FLFert_all)
 
-# the 456 length on the combined file seems suspiciously short
+# Note: the 456 length on the combined file seems suspiciously short -- check that out
 
-# next try to graph use over time for each company
+# next try to graph use over time (year) for each company
+
+# create a new df with only the licensee and year total columns
+FLFert_all2 = FLFert_all[, c(8, 1, 2, 36, 29, 22, 15, 7)]
+View(FLFert_all2)
+dim(FLFert_all2) #456 8
+colnames(FLFert_all2)
+
+# melt data to collapse columns for graphing
+FLFert_melt = melt(FLFert_all2, na.rm = FALSE, id = c("ID", "Licensee", "ReportingPlant"))
+colnames(FLFert_melt)
+dim(FLFert_melt) #2280 5
+names(FLFert_melt)[4] ="Year"
+names(FLFert_melt)[5] ="Tonnage"
+colnames(FLFert_melt)
+View(FLFert_melt)
+
+# graph data to look for major trends and plants to highlight
+#tiff(filename = "FLFertPlantUse.17.22.tiff", units = "in", compression = "lzw", res = 300, width = 12, height = 6)
+
+# this code copied and pasted below, still need to customize
+
+ggplot(aes(x = Year, y= Tonnage, group = ReportingPlant), data = FLFert_melt) +
+  geom_point(color = "grey") +
+  xlab("Growing Year (Start)") + ylab("Fertilizer Tonnage Used") +
+  geom_smooth(method = "lm", se=FALSE, color="darkgreen")
+# dev.off() # run this line after figure code to finish saving out figure to file
